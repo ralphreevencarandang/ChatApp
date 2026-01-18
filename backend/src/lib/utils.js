7745c355c8d424649e44c28b01752df658dc839d
ralphreevencarandang <rvncrndng@gmail.com>
2026-01-18 20:10:52 +0800

    change the secure cookies to true

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,10 +4,10 @@ export const generateToken = (userId, res)=>{
     const token = jwt.sign({id:userId}, process.env.JWT_SECRET, {expiresIn: '1d'});
     res.cookie('token', token, {
         httpOnly: true, //prevent XSS attacks cross-site scripting attacls
-        secure: process.env.NODE_ENV != 'development' , //
+        secure: true , //
         sameSite: 'strict', //CSRF attacks cross-site request forgery attacks
         maxAge: 1 * 24 * 60 * 60 * 1000
-    })
+    })  
 
     return token
 }
