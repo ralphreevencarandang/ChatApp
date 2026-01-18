0c48a3b9fd3deb6016ea4974e7e7088db087c676
ralphreevencarandang <rvncrndng@gmail.com>
2026-01-18 19:36:53 +0800

    update the cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true, limit: '10mb'}))
 
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173' , 'https://talkly-eosin.vercel.app'],
     credentials:true,
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
