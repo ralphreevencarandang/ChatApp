8078f040cceeb6e0dba5dd75de353b55c7382165
ralphreevencarandang <rvncrndng@gmail.com>
2026-01-18 20:16:50 +0800

    set the secure to false

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,7 +4,7 @@ export const generateToken = (userId, res)=>{
     const token = jwt.sign({id:userId}, process.env.JWT_SECRET, {expiresIn: '1d'});
     res.cookie('token', token, {
         httpOnly: true, //prevent XSS attacks cross-site scripting attacls
-        secure: true , //
+        secure: false , //
         sameSite: 'strict', //CSRF attacks cross-site request forgery attacks
         maxAge: 1 * 24 * 60 * 60 * 1000
     })  
